be3dcb65f745b210743298266a94679c27ca2920
Brett Mandler <brettmandler@gmail.com>
2019-01-05 01:25:25 -0500

    Update fish plugins

--- fish/functions/_pure_format_time.fish
+++ fish/functions/_pure_format_time.fish
@@ -1,11 +1,16 @@
-function _pure_format_time -d "Format milliseconds to a human readable format"
-    set --local milliseconds $argv[1]
+set fail 1
+
+function _pure_format_time \
+    --description="Format milliseconds to a human readable format" \
+    --argument-names milliseconds threshold
+    
+    if test $milliseconds -lt 0; return $fail; end
+
     set --local seconds (math -s0 "$milliseconds / 1000 % 60")
     set --local minutes (math -s0 "$milliseconds / 60000 % 60")
     set --local hours (math -s0 "$milliseconds / 3600000 % 24")
     set --local days (math -s0 "$milliseconds / 86400000")
     set --local time
-    set --local threshold $argv[2]
 
     if test $days -gt 0
         set time $time (printf "%sd" $days)
--- fish/functions/_pure_prompt_first_line.fish
+++ fish/functions/_pure_prompt_first_line.fish
@@ -1,23 +1,32 @@
-function _pure_prompt_first_line
-    set --local user_and_host (_pure_prompt_user_and_host)
+function _pure_prompt_first_line \
+    --description 'Print contextual information before prompt.'
 
-    # Exit with code 1 if git is not available
-    if not type -fq git
+    if not type -fq git  # exit if git is not available
         return 1
     end
 
-    set --local git_repo (_pure_prompt_git)
+    set --local prompt (_pure_print_prompt \
+                            (_pure_prompt_user_and_host) \
+                            (_pure_prompt_git) \
+                            (_pure_prompt_command_duration)
+                        )
+    set --local prompt_width (_pure_string_width $prompt)
+    set --local current_folder (_pure_prompt_current_folder $prompt_width)
 
-    set --local command_duration (_pure_prompt_command_duration)
-
-    set --local prompt (_pure_print_prompt $user_and_host $git_repo $command_duration)
-
-    set --local current_prompt_width (_pure_string_width $prompt)
-    set --local current_folder (_pure_prompt_current_folder $current_prompt_width)
-
-    if test $pure_user_host_location -eq 0
-        echo (_pure_print_prompt $current_folder $git_repo $user_and_host $command_duration)
+    set --local prompt_components
+    if test $pure_prompt_begin_with_current_directory = true
+        set prompt_components \
+                (_pure_prompt_current_folder $prompt_width) \
+                (_pure_prompt_git) \
+                (_pure_prompt_user_and_host) \
+                (_pure_prompt_command_duration)
     else
-        echo (_pure_print_prompt $user_and_host $current_folder $git_repo $command_duration)
+        set prompt_components \
+                (_pure_prompt_user_and_host) \
+                (_pure_prompt_current_folder $prompt_width) \
+                (_pure_prompt_git) \
+                (_pure_prompt_command_duration)
     end
+
+    echo (_pure_print_prompt $prompt_components)
 end
