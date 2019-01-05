90f270b8b06e478c6245ce132be2a73d5f44da0e
Brett Mandler <brettmandler@gmail.com>
2019-01-05 01:45:22 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 BASEDIR=$(dirname "$0")
 
 setupFish() {
-    if command -v fish > /dev/null; then
+    if command -v fish > /dev/null 2>&1; then
         echo "Installing fisher..."
         curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish > /dev/null
         echo "Installing plugins..."
-        fish -c fisher > /dev/null
+        fish -c fisher
         read -p "Do you wish to setup neovim (y/n)" yn
         case $yn in
             [Yy]* ) chsh -s /usr/bin/fish;;
@@ -19,22 +19,23 @@ setupFish() {
 }
 
 setupEmacs() {
-    if command -v emacs > /dev/null; then
+    if command -v emacs > /dev/null 2>&1; then
         cd "$HOME"/.emacs.d
-        make
+        cp init.example.el init.el
+        make install
     else
         echo "Please install emacs"
     fi
 }
 
 setupNvim() {
-    if command -v nvim > /dev/null; then
+    if command -v nvim > /dev/null 2>&1; then
         cd "$BASEDIR"/nvim
-        if command -v pip3 > /dev/null; then
+        if command -v pip3 > /dev/null 2>&1; then
             echo "Setting up python virtual environment..."
-            ./venv.sh > /dev/null
+            ./venv.sh
             echo "Installing plugins..."
-            make > /dev/null
+            make
             echo "Setting colorscheme..."
             nvim --cmd 'set t_ti= t_te= nomore' -N -U NONE -i NONE \
                 -c "try | colorscheme palenight | finally | call confirm('') | qall! | endtry"
@@ -47,7 +48,11 @@ setupNvim() {
     fi
 }
 
-if command -v go > /dev/null; then
+echo "Updating submodules..."
+
+git submodule update --recursive --init
+
+if command -v go > /dev/null 2>&1; then
     if test ! -x "$GOPATH"/bin/dotbro; then
         echo "Installing dotbro..."
         go get -u github.com/hypnoglow/dotbro
@@ -57,6 +62,10 @@ if command -v go > /dev/null; then
 else
     echo "Please install go. Cannot install dotbro"
     exit 1
+fi
+
+if command -v bat > /dev/null 2>&1; then
+    bat cache init
 fi
 
 read -p "Do you wish to setup neovim (y/n)" yn
